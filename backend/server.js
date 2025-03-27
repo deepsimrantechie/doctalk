976371772e720a8becf9d3c406eb5906863864b4
SIMRAN <118015518+deepsimrantechie@users.noreply.github.com>
2025-03-27 12:52:54 +0530

    added

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,9 @@ import connectDB from "./config/db.js";
 import UserRouter from "./router/authRouter.js";
 import BlogRouter from "./router/blogRoutes.js";
 import appointmentrouter from "./router/appointmentRouter.js";
-import path from "path";
 
 configDotenv();
 const app = express();
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, "frontend/build")));
 const port = process.env.PORT || 6002;
 const allowedOrigins = [
   "http://localhost:5173", // Development
@@ -35,9 +32,8 @@ app.use("/api/user", UserRouter);
 app.use("/api/blog", BlogRouter);
 app.use("/api/appointment", appointmentrouter);
 
-// Catch-all route to serve `index.html`
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+app.get("/", (req, res) => {
+  res.send("Api is working find ");
 });
 
 const startServer = () => {
